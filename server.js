ec54138995909909544e39d09a1a7f5ba22bb887
srxrt <str9703@gmail.com>
2024-08-14 10:43:41 +0900

    fix:removed some routes from server.js

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4: Routing
+
+// app.get("/hello", function (req, res) {
+// 	res.end("<h1>Hello World</h1>");
+// });
+
+// app.get("/gift", function (req, res) {
+// 	res.end("<h1>Siz sovgalar bolimidasiz</h1>");
+// });
+
 app.get("/", function (req, res) {
 	res.render("harid");
 });
