65231382c9db7223fc498ff61720cd8d573b809a
srxrt <str9703@gmail.com>
2024-08-14 11:50:31 +0900

    feat: added resume to /author route

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const http = require("http");
 const app = express();
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+	if (err) {
+		console.log("ERROR:", err);
+	} else {
+		user = JSON.parse(data);
+	}
+});
 
 //1: kirish kodlari
 app.use(express.static("public"));
@@ -30,6 +40,11 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
 	res.render("harid");
 });
+
+app.get("/author", (req, res) => {
+	res.render("author", { user: user });
+});
+
 app.post("/create-item", (req, res) => {
 	console.log(req.body);
 	res.json({ test: "success" });
