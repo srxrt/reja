33d96004d053c281f65361138db84df9112d8e88
srxrt <str9703@gmail.com>
2024-08-27 20:50:23 +0900

    MITASK-D solution

--- train.js
+++ train.js
@@ -117,7 +117,7 @@ function countDigits(string) {
 }
 
 console.log("Number of numbers:", countDigits("ad2a54y79wet0sfgb9"));
-*/
+
 
 //C-TASK:
 
@@ -135,3 +135,71 @@ function checkContent(str1, str2) {
 }
 
 console.log(checkContent("mitgroup", "gmtiprou"));
+*/
+
+// D-TASK:
+
+// Shunday class tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+class Shop {
+	#non;
+	#lagmon;
+	#cola;
+	constructor(non, lagmon, cola) {
+		this.#non = non;
+		this.#lagmon = lagmon;
+		this.#cola = cola;
+	}
+	#getDate() {
+		const date = new Date();
+		return `${date.getHours()}:${date.getMinutes()}`;
+	}
+
+	qoldiq() {
+		console.log(
+			`Hozir ${this.#getDate()}da ${this.#non}ta non, ${
+				this.#lagmon
+			}ta lagmon va ${this.#cola}ta cola mavjud!`
+		);
+	}
+	qabul(item, amount) {
+		switch (item) {
+			case "non":
+				this.#non += amount;
+				console.log(` ${this.#getDate()}da ${amount}ta non qoshildi`);
+				break;
+			case "lagmon":
+				this.#lagmon += amount;
+				console.log(` ${this.#getDate()}da ${amount}ta lagmon qoshildi`);
+				break;
+			default:
+				this.#cola += amount;
+				console.log(` ${this.#getDate()}da ${amount}ta cola qoshildi`);
+				break;
+		}
+	}
+	sotish(item, amount) {
+		switch (item) {
+			case "non":
+				this.#non -= amount;
+				console.log(` ${this.#getDate()}da ${amount}ta non sotildi.`);
+				break;
+			case "lagmon":
+				this.#lagmon -= amount;
+				console.log(` ${this.#getDate()}da ${amount}ta lagmon sotildi.`);
+				break;
+			default:
+				this.#cola -= amount;
+				console.log(` ${this.#getDate()}da ${amount}ta cola sotildi.`);
+				break;
+		}
+	}
+}
+
+const shop = new Shop(4, 5, 2);
+
+shop.qabul("cola", 5);
+shop.qoldiq();
+shop.sotish("non", 2);
+shop.qoldiq();
